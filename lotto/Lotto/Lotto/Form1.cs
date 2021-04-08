29bb7f197a6b96b3462e8e64e418a36f39f76af8
ehdrb613 <ehdrb0613@gmail.com>
2021-04-08 16:42:36 +0900

    dd

---
+++ lotto/Lotto/Lotto/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_test_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+
+            Random r = new Random();
+
+            MessageBox.Show(r.Next(1, 46).ToString());
+
+            labelNum1.Text = r.Next(10).ToString();
+            labelNum2.Text = r.Next(10).ToString();
+            labelNum3.Text = r.Next(10).ToString();
+            labelNum4.Text = r.Next(10).ToString();
+            labelNum5.Text = r.Next(10).ToString();
+            labelNum6.Text = r.Next(10).ToString();
+
+        }
+    }
+}
---
+++ lotto/Lotto/Lotto/json/LottoNumJson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Lotto.json
+{
+    class LottoNumJson
+    {
+
+        public void JsParse()
+        {
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                string jsonURL = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=957";
+
+            }
+        }
+    }
+
+  
+
+}
